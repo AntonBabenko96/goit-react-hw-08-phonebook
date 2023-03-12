3f1064deab0df96fc8b7c2483b7e12b3a1772a6b
AntonBabenko96 <ababenko159@gmail.com>
2023-03-13 00:36:17 +0200

    sws

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,28 @@
 import css from '../Home/Home.module.css';
+import logo from '../Home/phonebook.png';
+import { useSelector } from 'react-redux';
 
 export const Home = () => {
+  const isLogin = useSelector(state => state.auth.isLogin);
+  const name = useSelector(state => state.auth.user.name);
+
   return (
     <div className={css.home}>
-      <p>
-        Hello, user! Welcome to App PhoneBook. Please register or log in to gain
-        access to the full functionality. Our application allows you to add and
-        store contacts, and if necessary, you can filter or delete them.
-      </p>
+      <img src={logo} alt="logo" className={css.logo} />
+      {isLogin ? (
+        <p>
+          Hello, {name}! Welcome to App PhoneBook. Our application allows you to
+          add and store contacts, and if necessary, you can filter or delete
+          them.
+        </p>
+      ) : (
+        <p>
+          Hello, user! Welcome to App PhoneBook. Please register or log in to
+          gain access to the full functionality. Our application allows you to
+          add and store contacts, and if necessary, you can filter or delete
+          them.
+        </p>
+      )}
     </div>
   );
 };
